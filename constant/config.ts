4c2b56b1295ed5705f59ec932689618a1ca7d009
vickystickz <kademvicky@gmail.com>
2024-06-15 11:39:19 +0100

    Update attributions

--- constant/config.ts
+++ constant/config.ts
@@ -13,7 +13,7 @@ const APP_CONFIG: APP_CONFIG_TYPE = {
     projectStatus: "In Progress",
     percentage: 20.5,
 
-    CUSTOM_ATTRIBUTION: "Development by YouthMappers Dev. led by Victor Ademoyero",
+    CUSTOM_ATTRIBUTION: "Development by YouthMappers. Project led by Victor Ademoyero",
     MAP_CONFIG: {
         MAP_CENTER: [5.210266, 7.250771],
         MAP_STYLE: "mapbox://styles/mapbox/streets-v12"
