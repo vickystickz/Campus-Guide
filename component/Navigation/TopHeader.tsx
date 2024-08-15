6350a9d7fbf07cc5ba7a19f25947c63e80b9951f
vickystickz <kademvicky@gmail.com>
2024-08-15 10:38:35 +0100

    chore: Add mobile styling

--- component/Navigation/TopHeader.tsx
+++ component/Navigation/TopHeader.tsx
@@ -40,6 +40,7 @@ const TopHeader = () => {
   } = useAppContext();
   const { current: mymap } = useMap();
   const [usedCurrentLocation, setUsedCurrentLocation] = useState(false);
+  const [showModal, setShowModal] = useState(true);
 
   // route request
   const { data, isLoading, isError, error } = useGetRoute(
@@ -75,21 +76,21 @@ const TopHeader = () => {
         routeInfoAvailable
           ? "lg:top-0 md:h-full md:rounded-none"
           : "md:top-7 md:left-7 md:rounded-3xl md:h-fit"
-      } rounded-t-3xl rounded-b-none bg-white shadow-modal bottom-0 md:w-fit w-full z-50 flex`}
+      } rounded-t-3xl rounded-b-none bg-white flex-col overflow-hidden shadow-modal  transition-all delay-150 ease-in-out bottom-0 md:w-fit w-full z-50 flex`}
     >
-      <div className={`md:w-fit w-full flex flex-col`}>
-        {/* <div>
-        <Button
-          href="/"
-          className={`block p-0 ${
-            baseMap === MAP_CONFIG.MAP_STYLE.DEFAULT
-              ? "text-blue-300"
-              : "text-white"
-          }`}
-        >
-          <CG />
-        </Button>
-      </div> */}
+      <div
+        className="md:hidden flex items-center justify-center py-2 hover:cursor-pointer"
+        onClick={() => setShowModal(!showModal)}
+      >
+        <span className="bg-neutral-400 rounded-full h-[0.3rem] w-[4rem]"></span>
+      </div>
+      <div
+        className={`md:w-fit w-full flex-col transition-all duration-300 ease-in-out  ${
+          showModal
+            ? "max-h-[calc(100vh-2rem)] opacity-100 transform translate-y-0"
+            : "max-h-0 opacity-0 transform translate-y-full overflow-hidden"
+        }`}
+      >
         <div
           className={`flex flex-col gap-5 h-fit lg:w-[26rem] md:w-[24rem] w-full lg:px-10 lg:py-8 md:px-4 md:py-6 px-4 py-6`}
         >
@@ -104,7 +105,7 @@ const TopHeader = () => {
                 <Input
                   placeholder="Where are you right now?"
                   className="w-full"
-                  value={usedCurrentLocation ? "Your Location": ""}
+                  value={usedCurrentLocation ? "Your Location" : ""}
                 />
                 {startMarker && (
                   <CloseToastIcon
@@ -134,27 +135,27 @@ const TopHeader = () => {
             </div>
           </div>
           {!routeInfoAvailable && (
-                  <div className="flex items-center gap-4 mt-2 cursor-pointer"
-                  onClick={() => {
-                    navigator.geolocation.getCurrentPosition(
-                      (position) => {
-                        setStartMarker({
-                          latitude: position.coords.latitude,
-                          longitude: position.coords.longitude,
-                        });
-                        setUsedCurrentLocation(true);
-                      },
-                      (error) => {
-                        console.log(error);
-                      }
-                    );
-                  }}
-                >
-                  <GeoLocateIcon className="text-purple-600" />
-                  <span>Use my Current Location</span>
-                </div>
+            <div
+              className="flex items-center gap-4 mt-2 cursor-pointer"
+              onClick={() => {
+                navigator.geolocation.getCurrentPosition(
+                  (position) => {
+                    setStartMarker({
+                      latitude: position.coords.latitude,
+                      longitude: position.coords.longitude,
+                    });
+                    setUsedCurrentLocation(true);
+                  },
+                  (error) => {
+                    console.log(error);
+                  }
+                );
+              }}
+            >
+              <GeoLocateIcon className="text-purple-600" />
+              <span>Use my Current Location</span>
+            </div>
           )}
-      
         </div>
 
         {startMarker && endMarker && (
@@ -177,61 +178,34 @@ const TopHeader = () => {
                     </div>
                   </div>
                 )}
-                
               </div>
-            ): isLoading ? <RouteInfoSkeleton /> : null}
-
-            
+            ) : isLoading ? (
+              <RouteInfoSkeleton />
+            ) : null}
           </div>
         )}
-        {
-          routeInfoAvailable && (
-            <ul className="h-full overflow-auto space-y-4 font-sora px-8 py-2 mt-1 mb-4 ">
-                {
-                  routeInfo && routeInfo?.length > 0 && (
-                    routeInfo[0].legs[0].steps.map((step, index) => {
-                      return(
-                        <StepCard 
-                          intersections={step.intersections}
-                          maneuver={step.maneuver}
-                          name={step.name}
-                          mode={step.mode}
-                          duration={step.duration}
-                          distance={step.distance}
-                          driving_side={step.driving_side}
-                          weight={step.weight}
-                          geometry={step.geometry}
-                          key={index}
-                        />
-                      )
-                    }
-                  )
-                  )}
-              </ul>
-          )
-        }
-         
-        {/* {routeInfoAvailable && (
-          <div className="w-full shadow-modal md:py-2 py-2 flex items-center justify-center">
-            <Button
-              className="bg-purple-200 md:px-6 md:py-[1rem] gap-2.5 w-fit font-medium md:text-s-sm text-p-xs text-white"
-              icon={<StartNavigationIcon className="text-white" />}
-              iconPosition="left"
-              onClick={() => {}}
-            >
-              <span>Start</span>
-            </Button>
-          </div>
-        )} */}
-
-        {/* <Button
-        href="/"
-        className="bg-purple-300 font-medium w-fit h-fit md:text-p-base text-p-xs text-white"
-        icon={<RouteIcon />}
-        iconPosition="right"
-      >
-        <span>Contribute</span>
-      </Button> */}
+        {routeInfoAvailable && (
+          <ul className="md:h-full h-[15rem]  overflow-auto space-y-4 font-sora px-8 py-2 mt-1 mb-4 ">
+            {routeInfo &&
+              routeInfo?.length > 0 &&
+              routeInfo[0].legs[0].steps.map((step, index) => {
+                return (
+                  <StepCard
+                    intersections={step.intersections}
+                    maneuver={step.maneuver}
+                    name={step.name}
+                    mode={step.mode}
+                    duration={step.duration}
+                    distance={step.distance}
+                    driving_side={step.driving_side}
+                    weight={step.weight}
+                    geometry={step.geometry}
+                    key={index}
+                  />
+                );
+              })}
+          </ul>
+        )}
       </div>
     </div>
   );
